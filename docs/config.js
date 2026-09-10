ee7bccf931aa17bd80583873738b1ffe3ab205e4
Hans Sai <37721651+Spairkie@users.noreply.github.com>
2026-09-10 17:02:24 -0400

    Connect NOD to Cloudflare production

--- docs/config.js
+++ docs/config.js
@@ -1,5 +1,5 @@
 window.__NOD_CONFIG__ = {
-  API_BASE_URL: "",
-  SHORT_DOMAIN: "nod.link",
-  TURNSTILE_SITE_KEY: ""
+  API_BASE_URL: "https://nod-edge.saihanswissle.workers.dev",
+  SHORT_DOMAIN: "nod-edge.saihanswissle.workers.dev",
+  TURNSTILE_SITE_KEY: "0x4AAAAAAEvcoX_bTdRtl6pL"
 };
